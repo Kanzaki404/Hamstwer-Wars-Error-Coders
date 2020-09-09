a3583d6859c749ad2ab4bcdaef3a6ee6a72f98dc
Alexander <Alexander.risteski23@gmail.com>
2020-09-09 15:32:01 +0200

    nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+
+export default function Nav() {
+    return (
+        <div>
+            nav here
+        </div>
+    )
+}
