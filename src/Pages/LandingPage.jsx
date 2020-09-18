bd4329f7f8edb00b3475ad2ec3d34bb528480939
AngelicMob <angelic97.andersson@gmail.com>
2020-09-18 02:55:06 +0200

    added  animations

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -19,9 +19,9 @@ const LandingPageStyle = styled.div`
     font-family: "Quicksand", sans-serif;
     font-weight: 900;
     margin-bottom: 64px;
-    animation: vibrate-3;
-    animation-duration: 0.7s;
-    animation-iteration-count: infinite;
+    animation: fade-in;
+    animation-duration: 1.8s;
+
   }
 
   .text {
@@ -39,11 +39,13 @@ const LandingPageStyle = styled.div`
       font-weight: lighter;
       font-family: "Permanent Marker",cursive;
     }
-
-
     .goham-text {
         text-align: center;
         margin-bottom: -6px;
+        animation: puff-in-center;
+        animation-duration: 0.5s;
+
+
         span {
           font-weight: bold;
         }
@@ -138,7 +140,38 @@ const LandingPageStyle = styled.div`
     }
   }
 
-
+  @-webkit-keyframes puff-in-center {
+  0% {
+    -webkit-transform: scale(2);
+            transform: scale(2);
+    -webkit-filter: blur(4px);
+            filter: blur(4px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: scale(1);
+            transform: scale(1);
+    -webkit-filter: blur(0px);
+            filter: blur(0px);
+    opacity: 1;
+  }
+}
+@keyframes puff-in-center {
+  0% {
+    -webkit-transform: scale(2);
+            transform: scale(2);
+    -webkit-filter: blur(4px);
+            filter: blur(4px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: scale(1);
+            transform: scale(1);
+    -webkit-filter: blur(0px);
+            filter: blur(0px);
+    opacity: 1;
+  }
+}
 
 `;
 
